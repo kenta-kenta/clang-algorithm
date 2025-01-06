d4321f8283f3d59f563c923d0a26b86cc44fe7d3
kenta-kenta <kenken22352235@gmail.com>
2025-01-06 17:14:38 +0900

    HashTableの実装とテストの実装

--- 5_HashTable/hashTable.c
+++ 5_HashTable/hashTable.c
@@ -72,4 +72,43 @@ int hashDelete(int arr[], int key)
 
 int main()
 {
+    int arr[M] = {NULL};
+
+    hashInsert(arr, 25);
+    hashInsert(arr, 37);
+    hashInsert(arr, 18);
+    hashInsert(arr, 55);
+    hashInsert(arr, 22);
+    hashInsert(arr, 35);
+    hashInsert(arr, 50);
+    hashInsert(arr, 63);
+    hashInsert(arr, 69);
+    hashInsert(arr, 95);
+    hashInsert(arr, 100);
+    hashInsert(arr, 105);
+    hashInsert(arr, 110);
+
+    for (int i = 0; i < M; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    printf("Search 55: %d\n", hashSearch(arr, 55));
+    printf("Search 100: %d\n", hashSearch(arr, 100));
+    printf("Search 110: %d\n", hashSearch(arr, 110));
+    printf("Search 111: %d\n", hashSearch(arr, 111));
+
+    hashDelete(arr, 55);
+    hashDelete(arr, 100);
+    hashDelete(arr, 110);
+    hashDelete(arr, 111);
+
+    for (int i = 0; i < M; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
