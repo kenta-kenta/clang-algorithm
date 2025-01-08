be017e1e797687f94ec6f0be3e67fd8053268198
kenta-kenta <kenken22352235@gmail.com>
2025-01-08 09:41:54 +0900

    チェイン法の実装

--- 5_HashTable/hashTable.c
+++ 5_HashTable/hashTable.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define M 13
 
@@ -6,73 +7,101 @@ int hash(int key, int index)
 {
     return (key + index) % M;
 }
-
-int hashInsert(int arr[], int key)
+// チェイン法のハッシュ法
+typedef struct node
 {
-    int index = 0;
-    while (index < M)
+    int data;
+    struct node *next;
+} Node;
+
+// Nodeを出力する
+void printList(Node *head)
+{
+    while (head != NULL)
     {
-        int j = hash(key, index);
-        if (arr[j] == NULL)
-        {
-            arr[j] = key;
-            return j;
-        }
-        else
-        {
-            index++;
-        }
+        printf("%d \n", head->data);
+        head = head->next;
     }
 }
 
-int hashSearch(int arr[], int key)
+// Nodeを先頭に追加する
+void prepend(Node **head, int data)
 {
-    int index = 0;
-
-    while (index < M)
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = *head;
+    if (*head != NULL)
     {
-        int j = hash(key, index);
-        if (arr[j] == key)
-        {
-            return j;
-        }
-        else if (arr[j] == NULL)
-        {
-            return -1;
-        }
-        else
-        {
-            index++;
-        }
+        newNode->next = *head;
     }
+    *head = newNode;
 }
 
-int hashDelete(int arr[], int key)
+// Nodeを削除する
+void deleteNode(Node **head, int key)
 {
-    int index = 0;
+    Node *temp = *head, *prev;
 
-    while (index < M)
+    if (temp != NULL && temp->data == key)
     {
-        int j = hash(key, index);
-        if (arr[j] == key)
-        {
-            arr[j] = NULL;
-            return j;
-        }
-        else if (arr[j] == NULL)
-        {
-            return -1;
-        }
-        else
-        {
-            index++;
-        }
+        *head = temp->next;
+        free(temp);
+        return;
     }
+
+    while (temp != NULL && temp->data != key)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return;
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+// Nodeを検索する
+void search(Node *head, int key)
+{
+    Node *current = head;
+    while (current != NULL)
+    {
+        if (current->data == key)
+        {
+            printf("Found %d\n", current->data);
+            return;
+        }
+        current = current->next;
+    }
+    printf("Not Found\n");
+}
+
+int hashInsert(Node *arr[], int key)
+{
+    int j = key % M;
+    prepend(&arr[j], key);
+    return j;
+}
+
+int hashSearch(Node *arr[], int key)
+{
+    int j = key % M;
+    search(arr[j], key);
+    return j;
+}
+
+int hashDelete(Node *arr[], int key)
+{
+    int j = key % M;
+    deleteNode(&arr[j], key);
+    return j;
 }
 
 int main()
 {
-    int arr[M] = {NULL};
+    Node *arr[M] = {NULL};
 
     hashInsert(arr, 25);
     hashInsert(arr, 37);
@@ -90,9 +119,9 @@ int main()
 
     for (int i = 0; i < M; i++)
     {
-        printf("%d ", arr[i]);
+        printf("arr[%d]: ", i);
+        printList(arr[i]);
     }
-    printf("\n");
 
     printf("Search 55: %d\n", hashSearch(arr, 55));
     printf("Search 100: %d\n", hashSearch(arr, 100));
@@ -106,9 +135,113 @@ int main()
 
     for (int i = 0; i < M; i++)
     {
-        printf("%d ", arr[i]);
+        printf("arr[%d]: ", i);
+        printList(arr[i]);
     }
-    printf("\n");
 
     return 0;
 }
+
+// オープンアドレス方式のハッシュ法
+// int hashInsert(int arr[], int key)
+// {
+//     int index = 0;
+//     while (index < M)
+//     {
+//         int j = hash(key, index);
+//         if (arr[j] == NULL)
+//         {
+//             arr[j] = key;
+//             return j;
+//         }
+//         else
+//         {
+//             index++;
+//         }
+//     }
+// }
+
+// int hashSearch(int arr[], int key)
+// {
+//     int index = 0;
+//     while (index < M)
+//     {
+//         int j = hash(key, index);
+//         if (arr[j] == key)
+//         {
+//             return j;
+//         }
+//         else if (arr[j] == NULL)
+//         {
+//             return -1;
+//         }
+//         else
+//         {
+//             index++;
+//         }
+//     }
+// }
+
+// int hashDelete(int arr[], int key)
+// {
+//     int index = 0;
+//     while (index < M)
+//     {
+//         int j = hash(key, index);
+//         if (arr[j] == key)
+//         {
+//             arr[j] = NULL;
+//             return j;
+//         }
+//         else if (arr[j] == NULL)
+//         {
+//             return -1;
+//         }
+//         else
+//         {
+//             index++;
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int arr[M] = {NULL};
+//     hashInsert(arr, 25);
+//     hashInsert(arr, 37);
+//     hashInsert(arr, 18);
+//     hashInsert(arr, 55);
+//     hashInsert(arr, 22);
+//     hashInsert(arr, 35);
+//     hashInsert(arr, 50);
+//     hashInsert(arr, 63);
+//     hashInsert(arr, 69);
+//     hashInsert(arr, 95);
+//     hashInsert(arr, 100);
+//     hashInsert(arr, 105);
+//     hashInsert(arr, 110);
+
+//     for (int i = 0; i < M; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+
+//     printf("Search 55: %d\n", hashSearch(arr, 55));
+//     printf("Search 100: %d\n", hashSearch(arr, 100));
+//     printf("Search 110: %d\n", hashSearch(arr, 110));
+//     printf("Search 111: %d\n", hashSearch(arr, 111));
+
+//     hashDelete(arr, 55);
+//     hashDelete(arr, 100);
+//     hashDelete(arr, 110);
+//     hashDelete(arr, 111);
+
+//     for (int i = 0; i < M; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+
+//     return 0;
+// }
